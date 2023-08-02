75276d64ba99c79a81d1e5c3543a2b2b64c22bd4
Prince Kumar Sharma <ipreencekmr@gmail.com>
2023-08-02 12:39:24 +0530

    compose configured with home

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,6 @@ import ReactDOM from "react-dom";
 import Header from "emp_header/Header";
 import Footer from "emp_footer/Footer";
 import Container from "emp_container/Container";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faAtom } from "@fortawesome/free-solid-svg-icons";
 
 import "./index.css";
 
@@ -12,7 +10,6 @@ const App = () => (
   <React.Fragment>
     <Header></Header>
     <Container></Container>
-    <FontAwesomeIcon icon={faAtom}></FontAwesomeIcon>
     <Footer></Footer>
   </React.Fragment>
 );
